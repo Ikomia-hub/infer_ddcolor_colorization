89bb04048e749e1dd1bfbdebde7d4b2f8c89cfb0
ambroise <ambroise.berthe@ikomia.com>
2024-01-18 12:31:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+<div align="center">
+  <img src="https://raw.githubusercontent.com/Ikomia-hub/infer_ddcolor_colorization/main/images/icon.png" alt="Algorithm icon">
+  <h1 align="center">infer_ddcolor_colorization</h1>
+</div>
+<br />
+<p align="center">
+    <a href="https://github.com/Ikomia-hub/infer_ddcolor_colorization">
+        <img alt="Stars" src="https://img.shields.io/github/stars/Ikomia-hub/infer_ddcolor_colorization">
+    </a>
+    <a href="https://app.ikomia.ai/hub/">
+        <img alt="Website" src="https://img.shields.io/website/http/app.ikomia.ai/en.svg?down_color=red&down_message=offline&up_message=online">
+    </a>
+    <a href="https://github.com/Ikomia-hub/infer_ddcolor_colorization/blob/main/LICENSE.md">
+        <img alt="GitHub" src="https://img.shields.io/github/license/Ikomia-hub/infer_ddcolor_colorization.svg?color=blue">
+    </a>    
+    <br>
+    <a href="https://discord.com/invite/82Tnw9UGGc">
+        <img alt="Discord community" src="https://img.shields.io/badge/Discord-white?style=social&logo=discord">
+    </a> 
+</p>
+
+![computed](https://raw.githubusercontent.com/Ikomia-hub/infer_ddcolor_colorization/main/images/result.png)
+
+![original](https://unsplash.com/photos/CIian0EjHAU/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzA1NTc0Nzk0fA&force=true)
+Original picture made by <a href="https://unsplash.com/fr/@adamlittmandavis?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Adam Littman Davis</a> on <a href="https://unsplash.com/fr/photos/photo-en-niveaux-de-gris-de-montagnes-et-darbres-CIian0EjHAU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+## :rocket: Use with Ikomia API
+
+#### 1. Install Ikomia API
+
+We strongly recommend using a virtual environment. If you're not sure where to start, we offer a tutorial [here](https://www.ikomia.ai/blog/a-step-by-step-guide-to-creating-virtual-environments-in-python).
+
+```sh
+pip install ikomia
+```
+
+#### 2. Create your workflow
+
+```python
+from ikomia.dataprocess.workflow import Workflow
+from ikomia.utils.displayIO import display
+
+# Init your workflow
+wf = Workflow()
+
+# Add algorithm
+algo = wf.add_task(name="infer_ddcolor_colorization", auto_connect=True)
+
+# Run on your image  
+wf.run_on(url="https://raw.githubusercontent.com/Ikomia-hub/infer_ddcolor_colorization/main/images/original.jpg")
+
+display(algo.get_output(0).get_image())
+```
+
+## :sunny: Use with Ikomia Studio
+
+Ikomia Studio offers a friendly UI with the same features as the API.
+
+- If you haven't started using Ikomia Studio yet, download and install it from [this page](https://www.ikomia.ai/studio).
+
+- For additional guidance on getting started with Ikomia Studio, check out [this blog post](https://www.ikomia.ai/blog/how-to-get-started-with-ikomia-studio).
+
+## :mag: Explore algorithm outputs
+
+Every algorithm produces specific outputs, yet they can be explored them the same way using the Ikomia API. For a more in-depth understanding of managing algorithm outputs, please refer to the [documentation](https://ikomia-dev.github.io/python-api-documentation/advanced_guide/IO_management.html).
+
+```python
+from ikomia.dataprocess.workflow import Workflow
+
+# Init your workflow
+wf = Workflow()
+
+# Add algorithm
+algo = wf.add_task(name="infer_ddcolor_colorization", auto_connect=True)
+
+# Run on your image  
+wf.run_on(url="https://raw.githubusercontent.com/Ikomia-hub/infer_ddcolor_colorization/main/images/original.jpg")
+
+# Iterate over outputs
+for output in algo.get_outputs():
+    # Print information
+    print(output)
+    # Export it to JSON
+    output.to_json()
+```
---
+++ infer_ddcolor_colorization_process.py
@@ -0,0 +1,124 @@
+import copy
+from ikomia import core, dataprocess
+import cv2
+from modelscope.outputs import OutputKeys
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+import os
+
+# --------------------
+# - Class to handle the algorithm parameters
+# - Inherits PyCore.CWorkflowTaskParam from Ikomia API
+# --------------------
+class InferDdcolorColorizationParam(core.CWorkflowTaskParam):
+
+    def __init__(self):
+        core.CWorkflowTaskParam.__init__(self)
+        # Place default value initialization here
+        # Example : self.window_size = 25
+        self.update = True
+
+
+    def set_values(self, params):
+        # Set parameters values from Ikomia Studio or API
+        # Parameters values are stored as string and accessible like a python dict
+        # Example : self.window_size = int(params["window_size"])
+        self.update = True
+
+    def get_values(self):
+        # Send parameters values to Ikomia Studio or API
+        # Create the specific dict structure (string container)
+        params = {}
+        # Example : paramMap["window_size"] = str(self.window_size)
+        return params
+
+
+# --------------------
+# - Class which implements the algorithm
+# - Inherits PyCore.CWorkflowTask or derived from Ikomia API
+# --------------------
+class InferDdcolorColorization(dataprocess.C2dImageTask):
+
+    def __init__(self, name, param):
+        dataprocess.C2dImageTask.__init__(self, name)
+        # Add input/output of the algorithm here
+        # Example :  self.add_input(dataprocess.CImageIO())
+        #           self.add_output(dataprocess.CImageIO())
+        self.pipeline = None
+
+        # Create parameters object
+        if param is None:
+            self.set_param_object(InferDdcolorColorizationParam())
+        else:
+            self.set_param_object(copy.deepcopy(param))
+
+    def get_progress_steps(self):
+        # Function returning the number of progress steps for this algorithm
+        # This is handled by the main progress bar of Ikomia Studio
+        return 1
+
+    def run(self):
+        # Main function of your algorithm
+        # Call begin_task_run() for initialization
+        self.begin_task_run()
+
+        # Examples :
+        # Get input :
+        img_input = self.get_input(0)
+
+        # Get output :
+        img_output = self.get_output(0)
+
+        param = self.get_param_object()
+
+        if param.update or self.pipeline is None:
+            plugin_folder = os.path.dirname(os.path.abspath(__file__))
+            os.environ['MODELSCOPE_CACHE'] = os.path.join(plugin_folder, "cached_models")
+            self.pipeline = pipeline(Tasks.image_colorization, model='damo/cv_ddcolor_image-colorization')
+
+        result = self.pipeline(img_input.get_image())
+
+        img_output.set_image(result['output_img'][:, :, ::-1])
+
+        # Step progress bar (Ikomia Studio):
+        self.emit_step_progress()
+
+        # Call end_task_run() to finalize process
+        self.end_task_run()
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CTaskFactory from Ikomia API
+# --------------------
+class InferDdcolorColorizationFactory(dataprocess.CTaskFactory):
+
+    def __init__(self):
+        dataprocess.CTaskFactory.__init__(self)
+        # Set algorithm information/metadata here
+        self.info.name = "infer_ddcolor_colorization"
+        self.info.short_description = "Algorithm to colorize grayscale image"
+        # relative path -> as displayed in Ikomia Studio algorithm tree
+        self.info.path = "Plugins/Python"
+        self.info.version = "1.0.0"
+        # self.info.icon_path = "your path to a specific icon"
+        self.info.authors = "Kang, Xiaoyang and Yang, Tao and Ouyang, Wenqi and Ren, Peiran and Li, Lingzhi and Xie, Xuansong"
+        self.info.article = "DDColor: Towards Photo-Realistic Image Colorization via Dual Decoders"
+        self.info.journal = "Proceedings of the IEEE/CVF International Conference on Computer Vision"
+        self.info.year = 2023
+        self.info.license = "Apache-2.0"
+        # URL of documentation
+        self.info.documentation_link = ""
+        # Code source repository
+        self.info.repository = ""
+        self.info.original_repository = "https://github.com/piddnad/DDColor/tree/master"
+        # Keywords used for search
+        self.info.keywords = "color, restoration, colorisation"
+        # General type
+        self.info.algo_type = core.AlgoType.INFER
+        # Algorithms tasks
+        self.info.algo_tasks = "COLORIZATION"
+
+    def create(self, param=None):
+        # Create algorithm object
+        return InferDdcolorColorization(self.info.name, param)
